937f389637802bcd36ab808fcc04097701a5377f
KavindaHarshana <kavinda@silverstripers.com>
2019-08-23 17:09:23 +0530

    try

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,25 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected $page_name;
+
+    protected $url;
+
+    protected $views;
+
+    public function pageName()
+    {
+        return $this->page_name;
+    }
+
+    public function pageUrl()
+    {
+        return $this->url;
+    }
+
+    public function index()
+    {
+        return view($this->views);
+    }
 }
---
+++ app/Pages/Page.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Pages;
+
+
+class Page
+{
+
+}
